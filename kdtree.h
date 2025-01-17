857f99d471096e61a02565813b68fa2854abd7f8
samridh-dev <singhsam@grinnell.edu>
2025-01-16 20:15:47 -0600

    initial commit

--- kdtree.h
+++ kdtree.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2025 Samridh D. Singh
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef KD_TREE_H
 #define KD_TREE_H
 
@@ -169,15 +185,9 @@ KD__IMPL(_findnn)
 
 }
 
-/* ------------------------------------------------------------------------- */
-/* END                                                                       */
-/* ------------------------------------------------------------------------- */
-
 #undef KD__DIM
 #undef KD__TYPE_T
 #undef KD__TYPE_F
-
 #undef KD__PREFIX
-
 #undef KD__USE_STATIC
 #undef KD__LIB
